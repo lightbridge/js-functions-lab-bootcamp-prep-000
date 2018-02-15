ff039b1203ef87ee4f5db49589a3815e9806589c
Chad Bishop <connect@ltbrdg.com>
2018-02-15 21:09:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function happyHolidays(string){
 }
 
 function happyHolidaysTo(name){
-  console.log("Happy holidays," + (name) + "!");
+  return("Happy holidays," + (name) + "!");
 }
 
 function happyHolidayTo(holiday, name){
-  console.log("Happy" + (holiday) +"," + (name) + "!");
+  return("Happy" + (holiday) +"," + (name) + "!");
 }
 
 function holidayCountdown(days, holiday){
-  console.log("It's" + (days) + "days until" + (holiday) + "!");
+  return("It's" + (days) + "days until" + (holiday) + "!");
 }
